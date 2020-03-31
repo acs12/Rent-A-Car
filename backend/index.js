f8e5d4c445cb39b3a867d0e81ca0d9e2e2e1692d
manisharigala <arigalamanish@gmail.com>
2020-03-30 19:34:29 -0700

    Added loginRouter

--- backend/index.js
+++ backend/index.js
@@ -6,6 +6,9 @@ const port = 3000;
 
 const app = express();
 
+const loginRouter = require('./routes/loginRouter');
+app.use('/login', loginRouter);
+
 app.get('/', (req, res) => {
 res.send('Hello World')
 

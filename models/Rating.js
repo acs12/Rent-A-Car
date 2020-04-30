34efacaac4f9d748e01c9d3bf129954be6f30a58
brianhsu0603 <56208363+brianhsu0603@users.noreply.github.com>
2020-04-30 03:37:44 -0700

    Delete Rating.js

--- models/Rating.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-const Schema = mongoose.Schema 
-const ratingSchema =  new Schema({
-
-     rating:{ type: Number, required: true},
-     comment:{ type: String}
-});
-
-module.exports = mongoose.model('Rating',ratingSchema);
